f25971f5dd3b1d3a5cc83c551125536e2eebb161
vipcxj <vipcxj@126.com>
2023-01-12 15:47:36 +0800

    fix version

--- ipywebcam/_version.py
+++ ipywebcam/_version.py
@@ -4,5 +4,22 @@
 # Copyright (c) Xiaojing Chen.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 1, 0, 'dev')
-__version__ = ".".join(map(str, version_info))
+import re
+
+__version__ = "0.1.0.dev"
+
+def extract_version_info(ver: str) -> tuple:
+    res = re.match(r'(?P<major>\\d+)\\.(?P<minor>\\d+)\\.(?P<patch>\\d+)(?P<channel>a|b|rc|.dev)?', ver)
+    major = res.group('major')
+    minor = res.group('minor')
+    patch = res.group('patch')
+    channel = res.group('channel')
+    if channel:
+        if channel == '.dev':
+            return (major, minor, patch, 'dev')
+        else:
+            return (major, minor, patch, channel)
+    else:
+        return (major, minor, patch)
+
+version_info = extract_version_info(__version__)
