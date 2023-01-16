57a557a2d87c4a0813edb067b3ca620811f25661
vipcxj <vipcxj@126.com>
2023-01-16 18:14:24 +0800

    fix lint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,44 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended'
-  ],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.eslint.json',
-    sourceType: 'module'
-  },
   plugins: ['@typescript-eslint'],
   rules: {
-    '@typescript-eslint/no-unused-vars': ['warn', { args: 'none' }],
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-namespace': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
-    '@typescript-eslint/quotes': [
-      'error',
-      'single',
-      { avoidEscape: true, allowTemplateLiterals: false }
-    ],
     curly: ['error', 'all'],
     eqeqeq: 'error',
-    'prefer-arrow-callback': 'error'
-  }
+    'prefer-arrow-callback': 'error',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+
+      // As mentioned in the comments, you should extend TypeScript plugins here,
+      // instead of extending them outside the `overrides`.
+      // If you don't want to extend any rules, you don't need an `extends` attribute.
+      extends: [
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+
+      parserOptions: {
+        project: 'tsconfig.eslint.json',
+        sourceType: 'module',
+      },
+
+      rules: {
+        '@typescript-eslint/no-unused-vars': ['warn', { args: 'none' }],
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-namespace': 'off',
+        '@typescript-eslint/no-use-before-define': 'off',
+        '@typescript-eslint/quotes': [
+          'error',
+          'single',
+          { avoidEscape: true, allowTemplateLiterals: false },
+        ],
+        curly: ['error', 'all'],
+        eqeqeq: 'error',
+        'prefer-arrow-callback': 'error',
+      },
+    },
+  ],
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const path = require('path');
 const version = require('./package.json').version;
 
@@ -5,7 +6,7 @@ const version = require('./package.json').version;
 const rules = [
   { test: /\.ts$/, loader: 'ts-loader' },
   { test: /\.js$/, loader: 'source-map-loader' },
-  { test: /\.css$/, use: ['style-loader', 'css-loader']}
+  { test: /\.css$/, use: ['style-loader', 'css-loader'] },
 ];
 
 const watchOptions = {
@@ -17,7 +18,7 @@ const externals = ['@jupyter-widgets/base'];
 
 const resolve = {
   // Add '.ts' and '.tsx' as resolvable extensions.
-  extensions: [".webpack.js", ".web.js", ".ts", ".js"]
+  extensions: ['.webpack.js', '.web.js', '.ts', '.js'],
 };
 
 module.exports = [
@@ -36,7 +37,7 @@ module.exports = [
       publicPath: '',
     },
     module: {
-      rules: rules
+      rules: rules,
     },
     devtool: 'source-map',
     externals,
@@ -57,21 +58,20 @@ module.exports = [
   {
     entry: './src/index.ts',
     output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
-        libraryTarget: 'amd',
-        library: "ipywebcam",
-        publicPath: 'https://unpkg.com/ipywebcam@' + version + '/dist/'
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'dist'),
+      libraryTarget: 'amd',
+      library: 'ipywebcam',
+      publicPath: 'https://unpkg.com/ipywebcam@' + version + '/dist/',
     },
     devtool: 'source-map',
     module: {
-        rules: rules
+      rules: rules,
     },
     externals,
     resolve,
     watchOptions,
   },
-
 
   /**
    * Documentation widget bundle
@@ -83,16 +83,15 @@ module.exports = [
     output: {
       filename: 'embed-bundle.js',
       path: path.resolve(__dirname, 'docs', 'source', '_static'),
-      library: "ipywebcam",
-      libraryTarget: 'amd'
+      library: 'ipywebcam',
+      libraryTarget: 'amd',
     },
     module: {
-      rules: rules
+      rules: rules,
     },
     devtool: 'source-map',
     externals,
     resolve,
     watchOptions,
-  }
-
+  },
 ];
